87063fd8ea62911d5a9be4ea9eca9e0a7320eda3
Alvaro Polo <apoloval@gmail.com>
2015-05-13 20:03:17 +0200

    Hold block data as part of `Tape` contents

--- src/bin/tape.rs
+++ src/bin/tape.rs
@@ -8,6 +8,23 @@
 
 use std::io::Read;
 use std::io::Error as IoError;
+use std::str::from_utf8;
+
+#[derive(Debug)]
+pub struct Block { data: Vec<u8>, }
+
+impl Block {
+ 
+    pub fn is_bin_header(&self) -> bool {
+        self.data.len() >= 16 && 
+            self.data[..10] == [0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0]
+    }
+
+    pub fn file_name(&self) -> Option<&str> {
+        if self.is_bin_header() { from_utf8(&self.data[10..16]).ok() } 
+        else { None }
+    }
+}
 
 #[derive(Debug)]
 pub enum File {
@@ -17,6 +34,7 @@ pub enum File {
 
 #[derive(Debug)]
 pub struct Tape {
+    blocks: Vec<Block>,
 	files: Vec<File>,
 }
 
@@ -29,16 +47,6 @@ impl From<IoError> for LoadError {
 	fn from(e: IoError) -> LoadError { LoadError::Io(e) }
 }
 
-struct Block { data: Vec<u8>, }
-
-impl Block {
-
-    pub fn is_bin_header(&self) -> bool {
-        self.data.len() >= 16 && 
-            self.data[..10] == [0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0]
-    }
-}
-
 impl Tape {
 
 	#[allow(dead_code)]
@@ -49,14 +57,13 @@ impl Tape {
 	}
 
 	pub fn from_bytes(bytes: &[u8]) -> Result<Tape, LoadError> {
-        let blocks = Tape::parse_blocks(bytes);
-		let mut tape = Tape { files: vec![] };
+		let mut tape = Tape { blocks: Tape::parse_blocks(bytes), files: vec![] };
         let mut i = 0;
-        while i < blocks.len() {
-            let block = &blocks[i];
+        while i < tape.blocks.len() {
+            let block = &tape.blocks[i];
             if block.is_bin_header() {
-                let name = String::from_utf8(Vec::from(&block.data[10..16])).unwrap();
-                let content = &blocks[i+1].data;
+                let name = block.file_name().unwrap().to_string();
+                let content = &tape.blocks[i+1].data;
                 let begin = (content[0] as usize) | (content[1] as usize) << 8;
                 let end = (content[2] as usize) | (content[3] as usize) << 8;
                 let start = (content[4] as usize) | (content[5] as usize) << 8;
@@ -64,7 +71,7 @@ impl Tape {
                 tape.files.push(File::Bin(name, begin, end, start, Vec::from(data)));
                 i = i + 2;
             } else {
-                tape.files.push(File::Custom(blocks[i].data.clone()));
+                tape.files.push(File::Custom(block.data.clone()));
                 i = i + 1;
             }
         }
@@ -78,7 +85,7 @@ impl Tape {
 
         // First of all, we compute the indices of all block headers.
         for win in bytes.windows(8) {
-            if win == [0x1F, 0xA6, 0xDE, 0xBA, 0xCC, 0x13, 0x7D, 0x74] {
+            if win == [0x1f, 0xa6, 0xde, 0xba, 0xcc, 0x13, 0x7d, 0x74] {
                 hindex.push(i);
             }
             i = i + 1;
@@ -100,6 +107,21 @@ mod test {
 
     use super::*;
 
+    macro_rules! assert_bin {
+        ($f:expr, $n:expr, $b:expr, $e:expr, $s:expr, $d:expr) => {
+            match $f {
+            &File::Bin(ref name, begin, end, start, ref data) => {
+                assert_eq!($n, name);
+                assert_eq!($b, begin);
+                assert_eq!($e, end);
+                assert_eq!($s, start);
+                assert_eq!($d, &data[..]);
+            },
+            _ => panic!("unexpected file"),
+        }
+        }
+    }
+
     #[test]
     fn should_load_empty_tape() {
     	let bytes: Vec<u8> = vec![];
@@ -111,26 +133,16 @@ mod test {
     #[test]
     fn should_load_bin_file() {
         let bytes: Vec<u8> = vec![
-            0x1F, 0xA6, 0xDE, 0xBA, 0xCC, 0x13, 0x7D, 0x74,
+            0x1f, 0xa6, 0xde, 0xba, 0xcc, 0x13, 0x7d, 0x74,
             0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0, 0xd0,
             0x46, 0x4f, 0x4f, 0x42, 0x41, 0x52,
-            0x1F, 0xA6, 0xDE, 0xBA, 0xCC, 0x13, 0x7D, 0x74,
+            0x1f, 0xa6, 0xde, 0xba, 0xcc, 0x13, 0x7d, 0x74,
             0x00, 0x80, 0x08, 0x80, 0x00, 0x00,
             0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
         ];
-        let tape = Tape::from_bytes(&bytes);
-        assert!(tape.is_ok());
-        let files = tape.unwrap().files;
-        assert_eq!(1, files.len());
-        match &files[0] {
-            &File::Bin(ref name, begin, end, start, ref data) => {
-                assert_eq!("FOOBAR", name);
-                assert_eq!(0x8000, begin);
-                assert_eq!(0x8008, end);
-                assert_eq!(0x0000, start);
-                assert_eq!(&[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07], &data[..]);
-            },
-            _ => panic!("unexpected file"),
-        };
-    }
+        let tape = Tape::from_bytes(&bytes).unwrap();        
+        assert_eq!(1, tape.files.len());
+        assert_bin!(&tape.files[0], 
+            "FOOBAR", 0x8000, 0x8008, 0x0000, &[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]);
+    } 
 }
