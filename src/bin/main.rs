960842ff06ea258f450e72cdb86bb91c40b7cfff
Alvaro Polo <apoloval@gmail.com>
2015-05-14 21:04:49 +0200

    Link the list functionality to the main program

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,12 +12,14 @@ extern crate rustc_serialize;
 mod args;
 mod tape;
 
+use std::fs::File;
+
 #[allow(dead_code)]
 fn main() {
     let action = args::parse_args().action();
     match action {
         args::Action::Version => print_version(),
-        _ => {},
+        args::Action::List(path) => list_files(&path[..]),
     };
 }
 
@@ -27,4 +29,39 @@ fn print_version() {
     println!("");
     println!("This program is subject to the terms of the Mozilla Public License v2.0.");
     println!("");
+}
+
+fn list_files(path: &str) {
+    let mut tape_file = match File::open(path) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Cannot open file '{}': {}", path, e);
+            return
+        }
+    };
+    let tape = match tape::Tape::read(&mut tape_file) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Cannot read file '{}': {}", path, e);
+            return
+        }
+    };
+    for file in tape.files() {
+        match file {
+            tape::File::Bin(name, begin, end, start, data) => {
+                println!("bin    | {} | {:5} bytes | [0x{:x},0x{:x}]:0x{:x}", 
+                    name, data.len(), begin, end, start);
+            },
+            tape::File::Basic(name, data) => {
+                println!("basic  | {} | {:5} bytes", name, data.len());
+            },
+            tape::File::Ascii(name, data) => {
+                let nbytes = data.iter().fold(0, |size, chunk| size + chunk.len());
+                println!("ascii  | {} | {:5} bytes", name, nbytes);
+            },
+            tape::File::Custom(data) => {
+                println!("custom |        | {:5} bytes", data.len());
+            }
+        };
+    }
 }
